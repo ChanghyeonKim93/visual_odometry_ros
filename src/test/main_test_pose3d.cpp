cd44c9fbd569f1bb80c40f0d63e5974be5e06e0e
ChanghyeonKim93 <hyun91015@gmail.com>
2022-10-31 02:43:45 +0900

    rotation test is needed.

--- src/test/main_test_pose3d.cpp
+++ src/test/main_test_pose3d.cpp
@@ -56,8 +56,33 @@ int main()
     std::cout << pose.rotation().determinant() << std::endl;
 
 // 2) 
+    Rotation3 drot;
+    float angle = 2.0*3.1415926535897932384626433832795028841971693*0.001;
+    so3 w(angle,0,0);
+
+    drot << w;
+
+    std::cout << drot.R() << std::endl;
+    Rotation3 rot_tmp = rot;
+    for(int i = 0; i < 1000; ++i){
+        rot_tmp *= drot;
+
+        std::cout << i << "-th rot mult:\n";
+        std::cout << rot_tmp.R() << std::endl;
+        std::cout << rot_tmp.q() << std::endl;
+        std::cout << "determinant: " << rot_tmp.determinant() << std::endl;
+    }
 
 
+        std::cout << "original rot mult:\n";
+        std::cout << rot.R() << std::endl;
+        std::cout << rot.q() << std::endl;
+
+    Rotation3 rot_diff = rot * rot_tmp.inverse();
+    std::cout << rot_diff.q() << std::endl;
+// 0.306186 -0.250001  0.918557
+//  0.883883  0.433012 -0.176776
+// -0.353552  0.866024  0.353554
     std::cout << "End test.\n";
     return 0;
 };
