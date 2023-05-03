2a7b0e025421ffc6474c01418b7ba76e59d5f74e
ChanghyeonKim93 <hyun91015@gmail.com>
2023-05-03 22:34:59 +0900

    set 'release' & default topicnames are set.

--- ros2/visual_odometry/stereo_vo_ros2.cpp
+++ ros2/visual_odometry/stereo_vo_ros2.cpp
@@ -8,6 +8,10 @@ StereoVONode::StereoVONode(const std::string &node_name)
   // Get user pamareters
   this->getParameters();
 
+  // Make stereo vo object
+  std::string mode = "rosbag";
+  stereo_vo_ = std::make_unique<StereoVO>(mode, directory_intrinsic_);
+
   // Start ROS2 node
   timer_ = this->create_wall_timer(
       10ms, std::bind(&StereoVONode::callbackTimer, this));
@@ -29,7 +33,6 @@ StereoVONode::StereoVONode(const std::string &node_name)
   pub_pose_ = this->create_publisher<nav_msgs::msg::Odometry>(topicname_pose_, 10);
   pub_trajectory_ = this->create_publisher<nav_msgs::msg::Path>(topicname_trajectory_, 10);
   pub_map_points_ = this->create_publisher<sensor_msgs::msg::PointCloud2>(topicname_map_points_, 10);
-
   pub_debug_image_ = this->create_publisher<sensor_msgs::msg::Image>(topicname_debug_image_,10);
   // pub_debug_image_;
   std::cerr << "Stereo VO node generates publishers.\n";
@@ -55,20 +58,30 @@ void StereoVONode::callbackStereoImages(
 
 void StereoVONode::getParameters()
 {
-  if (!get_parameter_or<std::string>("/topicname_image_left", topicname_image_left_, "/left/image_raw"))
-    std::cerr << "'/topicname_image_left' is not set. Default is '/left/image_raw'\n";
-  if (!get_parameter_or<std::string>("/topicname_image_right", topicname_image_right_, "/right/image_raw"))
-    std::cerr << "'/topicname_image_right' is not set. Default is '/right/image_raw'\n";
+  topicname_image_left_ = "/left/image_raw";
+  topicname_image_right_ = "/right/image_raw";
+  topicname_pose_ = "/stereo_vo/pose";
+  topicname_map_points_ = "/stereo_vo/mappoints";
+  topicname_trajectory_ = "/stereo_vo/trajectory";
+  topicname_debug_image_ = "/stereo_vo/debug/image";
+
+  directory_intrinsic_ = "/home/kch/ros2_ws/src/visual_odometry_ros/config/stereo/exp_stereo2.yaml";
+
+  if (!get_parameter_or<std::string>("/topicname_image_left", topicname_image_left_, topicname_image_left_))
+    std::cerr << "'/topicname_image_left' is not set. Default is " << topicname_image_left_ << "\n";
+  if (!get_parameter_or<std::string>("/topicname_image_right", topicname_image_right_, topicname_image_right_))
+    std::cerr << "'/topicname_image_right' is not set. Default is " << topicname_image_right_ << "\n";
   if (!get_parameter_or<std::string>("/topicname_pose", topicname_pose_, topicname_pose_))
-    std::cerr << "'/topicname_pose' is not set. Default is '/-'\n";
+    std::cerr << "'/topicname_pose' is not set. Default is " << topicname_pose_ << "\n";
   if (!get_parameter_or<std::string>("/topicname_map_points", topicname_map_points_, topicname_map_points_))
-    std::cerr << "'/topicname_map_points' is not set. Default is '/-'\n";
+    std::cerr << "'/topicname_map_points' is not set. Default is " << topicname_map_points_ << "\n";
   if (!get_parameter_or<std::string>("/topicname_trajectory", topicname_trajectory_, topicname_trajectory_))
-    std::cerr << "'/topicname_trajectory' is not set. Default is '/-'\n";
+    std::cerr << "'/topicname_trajectory' is not set. Default is " << topicname_trajectory_ << "\n";
   if (!get_parameter_or<std::string>("/topicname_debug_image", topicname_debug_image_, "/stereo_vo/debug/image"))
-    std::cerr << "'/topicname_debug_image' is not set. Default is '/stereo_vo/debug/image'\n";
+    std::cerr << "'/topicname_debug_image' is not set. Default is " << topicname_debug_image_ << "\n";
+
   if (!get_parameter_or<std::string>("/directory_intrinsic", directory_intrinsic_, directory_intrinsic_))
-    std::cerr << "'/directory_intrinsic' is not set. Default is '/-'\n";
+    std::cerr << "'/directory_intrinsic' is not set. Default is "<< directory_intrinsic_ << "\n";
 
   std::cerr << "Stereo VO node gets parameters successfully!\n";
 }
