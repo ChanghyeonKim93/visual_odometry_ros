bb20d991428a777d017a8def97d843059e6403db
ChanghyeonKim93 <hyun91015@gmail.com>
2023-05-21 03:56:30 +0900

    delete spaces, delete turning  related variables

--- core/visual_odometry/frame.h
+++ core/visual_odometry/frame.h
@@ -28,7 +28,6 @@ private:
 
 private:
     uint32_t id_; // unique ID of this frame.
-
     double timestamp_; // timestamp of this frame in second.
 
     // Poses of the frame
@@ -52,15 +51,6 @@ private:
     bool is_keyframe_in_window_;
     bool is_poseonlyBA_success_; // 
 
-
-// For scale estimator
-private:
-    FramePtr frame_previous_turning_; // 직전 터닝 프레임.
-    float steering_angle_;
-    float scale_raw_; // observed raw scale
-    float scale_;  // refined scale
-    bool is_turning_frame_;
-
 // For stereo (right) frame
 private:
     bool is_right_image_; // stereo image.
@@ -70,15 +60,10 @@ public: // static counter.
     inline static uint32_t frame_counter_ = 0; // Unique frame counter. (static)
     inline static int max_pyr_lvl_ = 5;
 
-
-
-
-
-
 public:
-    Frame(const std::shared_ptr<Camera>& cam, bool is_right_image = false, const FramePtr& frame_left = nullptr);
+    Frame(const std::shared_ptr<Camera>& cam, bool is_right_image = false, FrameConstPtr& frame_left = nullptr);
     Frame(const std::shared_ptr<Camera>& cam, const cv::Mat& img, const double& timestamp,
-         bool is_right_image = false, const FramePtr& frame_left = nullptr);
+         bool is_right_image = false, FrameConstPtr& frame_left = nullptr);
 
     // Set
     void setPose(const PoseSE3& Twc);
@@ -96,37 +81,20 @@ public:
     const PoseSE3& getPoseInv() const;
     const PoseSE3& getPoseDiff10() const;
     const PoseSE3& getPoseDiff01() const;
-    const cv::Mat& getImage() const ; 
-    const cv::Mat& getImageDu() const ; 
-    const cv::Mat& getImageDv() const ; 
-    const CameraConstPtr& getCamera() const;
+    const cv::Mat& getImage() const; 
+    const cv::Mat& getImageDu() const; 
+    const cv::Mat& getImageDv() const; 
+    CameraConstPtr& getCamera() const;
     const LandmarkPtrVec& getRelatedLandmarkPtr() const;
     const PixelVec& getPtsSeen() const;
     const double& getTimestamp() const;
     bool isKeyframe() const;
     bool isKeyframeInWindow() const;
 
-
-// For scale estimator
-public:
-    void makeThisTurningFrame(const FramePtr& frame_previous_turning);
-    void setSteeringAngle(float steering_angle);
-    void setScaleRaw(float scale_raw);
-    void setScale(float scale);
-
-    const FramePtr& getPreviousTurningFrame() const;
-    const float&    getSteeringAngle() const;
-    const float&    getScaleRaw() const;
-    const float&    getScale() const;
-
-    bool isTurningFrame() const;
-    void cancelThisTurningFrame();
-
-
 // For stereo frame
 public:
     bool isRightImage() const;
-    const FramePtr& getLeftFramePtr() const;
+    FrameConstPtr& getLeftFramePtr() const;
 
 // For motion update flags
 public:
@@ -144,7 +112,7 @@ private:
     FramePtr right_; // frame pointr to upper frame (same as right frame)
 
 public:
-    StereoFrame(const FramePtr& frame_left, const FramePtr& frame_right);
+    StereoFrame(FrameConstPtr& frame_left, FrameConstPtr& frame_right);
     StereoFrame(const cv::Mat& img_left, const cv::Mat& img_right, 
         CameraConstPtr& cam_left, CameraConstPtr& cam_right, double timestamp);
 
