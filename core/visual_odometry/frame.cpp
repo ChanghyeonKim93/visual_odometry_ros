19be384c615a33b967a6591298515e06329406d3
ChanghyeonKim93 <hyun91015@gmail.com>
2023-08-15 15:41:56 +0900

    delete cv::Mat image in Frame.

--- core/visual_odometry/frame.cpp
+++ core/visual_odometry/frame.cpp
@@ -1,166 +1,161 @@
 #include "core/visual_odometry/frame.h"
 
-Frame::Frame(CameraConstPtr& cam, bool is_right_image, FrameConstPtr& frame_left)
-: is_keyframe_(false), is_keyframe_in_window_(false), is_poseonlyBA_success_(true)
+Frame::Frame(CameraConstPtr &cam, bool is_right_image, FrameConstPtr &frame_left)
+    : is_keyframe_(false), is_keyframe_in_window_(false), is_poseonlyBA_success_(true)
 {
-    cam_ = cam;
+  cam_ = cam;
 
-    Twc_ = PoseSE3::Identity();
-    Tcw_ = PoseSE3::Identity();
+  Twc_ = PoseSE3::Identity();
+  Tcw_ = PoseSE3::Identity();
 
-    dT01_ = PoseSE3::Identity();
-    dT10_ = PoseSE3::Identity();
+  dT01_ = PoseSE3::Identity();
+  dT10_ = PoseSE3::Identity();
 
-    timestamp_      = 0.0;
-    id_             = frame_counter_++;
+  timestamp_ = 0.0;
+  id_ = frame_counter_++;
 
-    // Stereo right image only.
-    is_right_image_ = is_right_image;
-    frame_left_     = frame_left;
-};
+  // Stereo right image only.
+  is_right_image_ = is_right_image;
+  frame_left_ = frame_left;
+}
 
-Frame::Frame(CameraConstPtr& cam, const cv::Mat& img, const double& timestamp,
-         bool is_right_image, FrameConstPtr& frame_left)
-: is_keyframe_(false), is_keyframe_in_window_(false), is_poseonlyBA_success_(true)
+Frame::Frame(CameraConstPtr &cam, const double &timestamp,
+             bool is_right_image, FrameConstPtr &frame_left)
+    : is_keyframe_(false), is_keyframe_in_window_(false), is_poseonlyBA_success_(true)
 {
-    cam_ = cam;
+  cam_ = cam;
 
-    Twc_ = PoseSE3::Identity();
-    Tcw_ = PoseSE3::Identity();
+  Twc_ = PoseSE3::Identity();
+  Tcw_ = PoseSE3::Identity();
 
-    dT01_ = PoseSE3::Identity();
-    dT10_ = PoseSE3::Identity();
+  dT01_ = PoseSE3::Identity();
+  dT10_ = PoseSE3::Identity();
 
-    timestamp_      = 0.0;
-    id_             = frame_counter_++;
+  timestamp_ = 0.0;
+  id_ = frame_counter_++;
 
-    this->setImageAndTimestamp(img, timestamp);
+  this->setTimestamp(timestamp);
 
-    // Stereo right image only.
-    is_right_image_ = is_right_image;
-    frame_left_     = frame_left;
-};
+  // Stereo right image only.
+  is_right_image_ = is_right_image;
+  frame_left_ = frame_left;
+}
 
-void Frame::setPose(const PoseSE3& Twc) { 
-    Twc_ = Twc; 
-    Tcw_ = geometry::inverseSE3_f(Twc_);
-};
-
-void Frame::setPoseDiff10(const Eigen::Matrix4f& dT10){
-    dT10_ = dT10;
-    dT01_ = geometry::inverseSE3_f(dT10);
-};
-
-void Frame::setImageAndTimestamp(const cv::Mat& img, const double& timestamp) { 
-    img.copyTo(image_); // CV_8UC1
-    timestamp_ = timestamp;
-    
-    // Calculate diff images
-    int kerner_size = 3;
-    cv::Sobel(image_, image_du_, CV_32FC1, 1, 0, kerner_size, 1.0, 0.0, cv::BORDER_DEFAULT);
-    cv::Sobel(image_, image_dv_, CV_32FC1, 0, 1, kerner_size, 1.0, 0.0, cv::BORDER_DEFAULT);
-};
-
-void Frame::setPtsSeenAndRelatedLandmarks(const PixelVec& pts, const LandmarkPtrVec& landmarks){
-    if(pts.size() != landmarks.size())
-        throw std::runtime_error("In 'Frame::setPtsSeenAndRelatedLandmarks()', pts.size() != landmarks.size()");
-
-    // pts_seen
-    pts_seen_.resize(pts.size());
-    std::copy(pts.begin(), pts.end(), pts_seen_.begin());
-
-    // related landmarks
-    related_landmarks_.resize(landmarks.size());
-    std::copy(landmarks.begin(), landmarks.end(), related_landmarks_.begin());
-};
-
-void Frame::makeThisKeyframe(){
-    is_keyframe_           = true;
-    is_keyframe_in_window_ = true;
-};
-
-void Frame::outOfKeyframeWindow(){
-    is_keyframe_in_window_ = false;
-};
-
-const uint32_t& Frame::getID() const { 
-    return id_; 
-};
-const PoseSE3& Frame::getPose() const { 
-    return Twc_; 
-};
-const PoseSE3& Frame::getPoseInv() const{
-    return Tcw_;
-};
-
-const PoseSE3& Frame::getPoseDiff10() const{
-    return dT10_;
-};
-const PoseSE3& Frame::getPoseDiff01() const{
-    return dT01_;
-};
-
-const cv::Mat& Frame::getImage() const {
-    return image_; 
-}; 
-
-const cv::Mat& Frame::getImageDu() const {
-    return image_du_;
-};
-
-const cv::Mat& Frame::getImageDv() const {
-    return image_dv_;
-};
-
-CameraConstPtr& Frame::getCamera() const {
-    return cam_;
-};
-
-const LandmarkPtrVec& Frame::getRelatedLandmarkPtr() const { 
-    return related_landmarks_; 
-};
-
-const PixelVec& Frame::getPtsSeen() const {
-    return pts_seen_;  
-};
-
-const double& Frame::getTimestamp() const {
-    return timestamp_;
-};
-
-bool Frame::isKeyframe() const{
-    return is_keyframe_;
-};
-bool Frame::isKeyframeInWindow() const{
-    return is_keyframe_in_window_;
-};
-
-bool Frame::isRightImage() const {
-    return is_right_image_;
-};
-
-FrameConstPtr& Frame::getLeftFramePtr() const
+void Frame::setPose(const PoseSE3 &Twc)
 {
-    if(!is_right_image_) 
-        throw std::runtime_error("In Frame::getLeftFramePtr() : This frame is not a right frame of a stereo camera!");
-    
-    return frame_left_;
-};
+  Twc_ = Twc;
+  Tcw_ = geometry::inverseSE3_f(Twc_);
+}
+
+void Frame::setPoseDiff10(const Eigen::Matrix4f &dT10)
+{
+  dT10_ = dT10;
+  dT01_ = geometry::inverseSE3_f(dT10);
+}
+
+void Frame::setTimestamp(const double &timestamp)
+{
+  timestamp_ = timestamp;
+}
+
+void Frame::setPtsSeenAndRelatedLandmarks(const PixelVec &pts, const LandmarkPtrVec &landmarks)
+{
+  if (pts.size() != landmarks.size())
+    throw std::runtime_error("In 'Frame::setPtsSeenAndRelatedLandmarks()', pts.size() != landmarks.size()");
+
+  // pts_seen
+  pts_seen_.resize(pts.size());
+  std::copy(pts.begin(), pts.end(), pts_seen_.begin());
+
+  // related landmarks
+  related_landmarks_.resize(landmarks.size());
+  std::copy(landmarks.begin(), landmarks.end(), related_landmarks_.begin());
+}
+
+void Frame::makeThisKeyframe()
+{
+  is_keyframe_ = true;
+  is_keyframe_in_window_ = true;
+}
+
+void Frame::outOfKeyframeWindow()
+{
+  is_keyframe_in_window_ = false;
+}
+
+const uint32_t &Frame::getID() const
+{
+  return id_;
+}
+const PoseSE3 &Frame::getPose() const
+{
+  return Twc_;
+}
+const PoseSE3 &Frame::getPoseInv() const
+{
+  return Tcw_;
+}
+
+const PoseSE3 &Frame::getPoseDiff10() const
+{
+  return dT10_;
+}
+const PoseSE3 &Frame::getPoseDiff01() const
+{
+  return dT01_;
+}
+
+CameraConstPtr &Frame::getCamera() const
+{
+  return cam_;
+}
+
+const LandmarkPtrVec &Frame::getRelatedLandmarkPtr() const
+{
+  return related_landmarks_;
+}
+
+const PixelVec &Frame::getPtsSeen() const
+{
+  return pts_seen_;
+}
+
+const double &Frame::getTimestamp() const
+{
+  return timestamp_;
+}
+
+bool Frame::isKeyframe() const
+{
+  return is_keyframe_;
+}
+bool Frame::isKeyframeInWindow() const
+{
+  return is_keyframe_in_window_;
+}
+
+bool Frame::isRightImage() const
+{
+  return is_right_image_;
+}
+
+FrameConstPtr &Frame::getLeftFramePtr() const
+{
+  if (!is_right_image_)
+    throw std::runtime_error("In Frame::getLeftFramePtr() : This frame is not a right frame of a stereo camera!");
+
+  return frame_left_;
+}
 
 bool Frame::isPoseOnlySuccess() const
 {
-    return is_poseonlyBA_success_;
-};
+  return is_poseonlyBA_success_;
+}
 
 void Frame::setPoseOnlyFailed()
 {
-    is_poseonlyBA_success_ = false;
-};
-
-
-
-
-
+  is_poseonlyBA_success_ = false;
+}
 
 /*
 ====================================================================================
@@ -172,37 +167,36 @@ void Frame::setPoseOnlyFailed()
 
 // stereo frame
 
-StereoFrame::StereoFrame(FrameConstPtr& f_l, FrameConstPtr& f_r)
+StereoFrame::StereoFrame(FrameConstPtr &f_l, FrameConstPtr &f_r)
 {
-    left_ = f_l;
-    right_ = f_r;
-};
+  left_ = f_l;
+  right_ = f_r;
+}
 
-StereoFrame::StereoFrame(const cv::Mat& img_left, const cv::Mat& img_right, CameraConstPtr& cam_left, CameraConstPtr& cam_right, double timestamp)
+StereoFrame::StereoFrame(CameraConstPtr &cam_left, CameraConstPtr &cam_right, double timestamp)
 {
-	left_  = std::make_shared<Frame>(cam_left, img_left, timestamp);
-	right_ = std::make_shared<Frame>(cam_right, img_right, timestamp, true, left_);
-};
+  left_ = std::make_shared<Frame>(cam_left, timestamp);
+  right_ = std::make_shared<Frame>(cam_right, timestamp, true, left_);
+}
 
-
-FrameConstPtr& StereoFrame::getLeft() const
+FrameConstPtr &StereoFrame::getLeft() const
 {
-    return left_;
-};
+  return left_;
+}
 
-FrameConstPtr& StereoFrame::getRight() const
+FrameConstPtr &StereoFrame::getRight() const
 {
-    return right_;
-};
+  return right_;
+}
 
-void StereoFrame::setStereoPoseByLeft(const PoseSE3& Twc_left, const PoseSE3& T_lr)
+void StereoFrame::setStereoPoseByLeft(const PoseSE3 &Twc_left, const PoseSE3 &T_lr)
 {
-    left_->setPose(Twc_left);
-    right_->setPose(Twc_left * T_lr);
-};
+  left_->setPose(Twc_left);
+  right_->setPose(Twc_left * T_lr);
+}
 
-void StereoFrame::setStereoPtsSeenAndRelatedLandmarks(const PixelVec& pts_l1, const PixelVec& pts_r1, const LandmarkPtrVec& lms)
+void StereoFrame::setStereoPtsSeenAndRelatedLandmarks(const PixelVec &pts_l1, const PixelVec &pts_r1, const LandmarkPtrVec &lms)
 {
-    left_->setPtsSeenAndRelatedLandmarks(pts_l1, lms);
-    right_->setPtsSeenAndRelatedLandmarks(pts_r1, lms);
-};  
+  left_->setPtsSeenAndRelatedLandmarks(pts_l1, lms);
+  right_->setPtsSeenAndRelatedLandmarks(pts_r1, lms);
+}
--- core/visual_odometry/frame.h
+++ core/visual_odometry/frame.h
@@ -19,108 +19,97 @@ class Frame;
 class StereoFrame;
 
 /// @brief Image frame class
-class Frame 
+class Frame
 {
 private:
-    std::shared_ptr<Camera> cam_; // camera class pointer
+  std::shared_ptr<Camera> cam_; // camera class pointer
 
 private:
-    uint32_t id_; // unique ID of this frame.
-    double timestamp_; // timestamp of this frame in second.
+  uint32_t id_;      // unique ID of this frame.
+  double timestamp_; // timestamp of this frame in second.
 
-    // Poses of the frame
-    PoseSE3 Twc_; // SE(3) w.r.t. the global frame {W}.
-    PoseSE3 Tcw_; // Inverse of Twc_.
+  // Poses of the frame
+  PoseSE3 Twc_; // SE(3) w.r.t. the global frame {W}.
+  PoseSE3 Tcw_; // Inverse of Twc_.
 
-    PoseSE3 dT10_;
-    PoseSE3 dT01_;
+  PoseSE3 dT10_;
+  PoseSE3 dT01_;
 
-    // Images of the frame
-    cv::Mat image_; // image (uint8)
-    cv::Mat image_du_; // derivative image of image along pixel u-axis.
-    cv::Mat image_dv_; // derivative image of image along pixel v-axis.
+  // Feature related storages
+  LandmarkPtrVec related_landmarks_;
+  PixelVec pts_seen_;
 
-    // Feature related storages
-    LandmarkPtrVec related_landmarks_;
-    PixelVec pts_seen_;
+  // Flags
+  bool is_keyframe_;
+  bool is_keyframe_in_window_;
+  bool is_poseonlyBA_success_; //
 
-// Flags
-    bool is_keyframe_;
-    bool is_keyframe_in_window_;
-    bool is_poseonlyBA_success_; // 
-
-// For stereo (right) frame
+  // For stereo (right) frame
 private:
-    bool is_right_image_; // stereo image.
-    FramePtr frame_left_; // Frame pointer to 'left frame' of this right frame. In this project, we consider the 'left image' is the reference image of the stereo frames. All poses of the right frames are represented by their 'left frames'.
-    
-public: // static counter.
-    inline static uint32_t frame_counter_ = 0; // Unique frame counter. (static)
-    inline static int max_pyr_lvl_ = 5;
+  bool is_right_image_; // stereo image.
+  FramePtr frame_left_; // Frame pointer to 'left frame' of this right frame. In this project, we consider the 'left image' is the reference image of the stereo frames. All poses of the right frames are represented by their 'left frames'.
+
+public:                                      // static counter.
+  inline static uint32_t frame_counter_ = 0; // Unique frame counter. (static)
+  inline static int max_pyr_lvl_ = 5;
 
 public:
-    Frame(const std::shared_ptr<Camera>& cam, bool is_right_image = false, FrameConstPtr& frame_left = nullptr);
-    Frame(const std::shared_ptr<Camera>& cam, const cv::Mat& img, const double& timestamp,
-         bool is_right_image = false, FrameConstPtr& frame_left = nullptr);
+  Frame(const std::shared_ptr<Camera> &cam, bool is_right_image = false, FrameConstPtr &frame_left = nullptr);
+  Frame(const std::shared_ptr<Camera> &cam, const double &timestamp,
+        bool is_right_image = false, FrameConstPtr &frame_left = nullptr);
 
-    // Set
-    void setPose(const PoseSE3& Twc);
-    void setPoseDiff10(const PoseSE3& dT10);
-    void setImageAndTimestamp(const cv::Mat& img, const double& timestamp); 
+  // Set
+  void setPose(const PoseSE3 &Twc);
+  void setPoseDiff10(const PoseSE3 &dT10);
+  void setTimestamp(const double &timestamp);
 
-    void setPtsSeenAndRelatedLandmarks(const PixelVec& pts, const LandmarkPtrVec& landmarks);
-    
-    void makeThisKeyframe();
-    void outOfKeyframeWindow();
+  void setPtsSeenAndRelatedLandmarks(const PixelVec &pts, const LandmarkPtrVec &landmarks);
 
-    // Get
-    const uint32_t& getID() const;
-    const PoseSE3& getPose() const;
-    const PoseSE3& getPoseInv() const;
-    const PoseSE3& getPoseDiff10() const;
-    const PoseSE3& getPoseDiff01() const;
-    const cv::Mat& getImage() const; 
-    const cv::Mat& getImageDu() const; 
-    const cv::Mat& getImageDv() const; 
-    CameraConstPtr& getCamera() const;
-    const LandmarkPtrVec& getRelatedLandmarkPtr() const;
-    const PixelVec& getPtsSeen() const;
-    const double& getTimestamp() const;
-    bool isKeyframe() const;
-    bool isKeyframeInWindow() const;
+  void makeThisKeyframe();
+  void outOfKeyframeWindow();
 
-// For stereo frame
+  // Get
+  const uint32_t &getID() const;
+  const PoseSE3 &getPose() const;
+  const PoseSE3 &getPoseInv() const;
+  const PoseSE3 &getPoseDiff10() const;
+  const PoseSE3 &getPoseDiff01() const;
+  CameraConstPtr &getCamera() const;
+  const LandmarkPtrVec &getRelatedLandmarkPtr() const;
+  const PixelVec &getPtsSeen() const;
+  const double &getTimestamp() const;
+  bool isKeyframe() const;
+  bool isKeyframeInWindow() const;
+
+  // For stereo frame
 public:
-    bool isRightImage() const;
-    FrameConstPtr& getLeftFramePtr() const;
+  bool isRightImage() const;
+  FrameConstPtr &getLeftFramePtr() const;
 
-// For motion update flags
+  // For motion update flags
 public:
-    bool isPoseOnlySuccess() const;
-    void setPoseOnlyFailed();
+  bool isPoseOnlySuccess() const;
+  void setPoseOnlyFailed();
 };
-
-
 
 /// @brief Stereo frame structure. (not used now.)
 class StereoFrame
 {
 private:
-    FramePtr left_; // frame pointr to lower frame (same as left frame)
-    FramePtr right_; // frame pointr to upper frame (same as right frame)
+  FramePtr left_;  // frame pointr to lower frame (same as left frame)
+  FramePtr right_; // frame pointr to upper frame (same as right frame)
 
 public:
-    StereoFrame(FrameConstPtr& frame_left, FrameConstPtr& frame_right);
-    StereoFrame(const cv::Mat& img_left, const cv::Mat& img_right, 
-        CameraConstPtr& cam_left, CameraConstPtr& cam_right, double timestamp);
+  StereoFrame(FrameConstPtr &frame_left, FrameConstPtr &frame_right);
+  StereoFrame(CameraConstPtr &cam_left, CameraConstPtr &cam_right, double timestamp);
 
 public:
-    FrameConstPtr& getLeft() const;
-    FrameConstPtr& getRight() const;
+  FrameConstPtr &getLeft() const;
+  FrameConstPtr &getRight() const;
 
 public:
-    void setStereoPoseByLeft(const PoseSE3& Twc_left, const PoseSE3& T_lr);
-    void setStereoPtsSeenAndRelatedLandmarks(const PixelVec& pts_l1, const PixelVec& pts_r1, const LandmarkPtrVec& lms);
+  void setStereoPoseByLeft(const PoseSE3 &Twc_left, const PoseSE3 &T_lr);
+  void setStereoPtsSeenAndRelatedLandmarks(const PixelVec &pts_l1, const PixelVec &pts_r1, const LandmarkPtrVec &lms);
 };
 
 #endif
